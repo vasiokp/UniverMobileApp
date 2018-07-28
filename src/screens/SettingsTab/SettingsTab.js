3d4d927f40b00ba64cd65e66ad01fccb4d8ec749
Vasyl Poklitar <vasiok26@gmail.com>
2018-07-28 19:54:57 +0300

    Implemented react-native-navigation and added react-native-vector-icons

---
+++ src/screens/SettingsTab/SettingsTab.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+class SettingsTabScreen extends Component {
+  render(){
+    return (
+        <View>
+          <Text>User Settings</Text>
+        </View>
+    );
+  }
+}
+
+export default SettingsTabScreen;
