7b17f60254e4d61922f385306f04cf5423480cb9
Vasyl Poklitar <vasiok26@gmail.com>
2018-09-01 13:09:44 +0300

    added layout for android

--- android/app/src/main/java/com/univermobileapp/MainActivity.java
+++ android/app/src/main/java/com/univermobileapp/MainActivity.java
@@ -1,7 +1,24 @@
 package com.univermobileapp;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 public class MainActivity extends SplashActivity {
-
+  @Override
+  public LinearLayout createSplashLayout(){
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+    textView.setBackgroundColor(Color.parseColor("#F89554"));
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextColor(Color.parseColor("#ffffff"));
+    textView.setText("Univer app");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,40);
+    view.addView(textView);
+    return view;
+  }
 }
