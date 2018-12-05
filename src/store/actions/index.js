72610c2b59d841ef7b201a8629b5ccae3ec5bb57
Vasyl Poklitar <poklitar@glooni.com>
2018-12-05 17:41:12 +0200

    fix after rebase

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,7 +1,4 @@
 export { fetchSchedule } from './schedule'
 export { fetchScheduleTypes } from './scheduleType'
-<<<<<<< HEAD
 export { fetchScheduleDetails } from './scheduleDetails'
-=======
 export { getNews } from './news'
->>>>>>> Updated News tab
